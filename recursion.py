50ae58699472e090d2ee47da8fa3a64239429677
SoraInkling72 <CherubimKen.Fonacier72@spsmail.cuny.edu>
2024-04-18 15:55:10 -0400

    WIP

--- recursion.py
+++ recursion.py
@@ -17,10 +17,62 @@ def gcd(a, b):
 
 
 def compareTo(s1, s2):
-    if s1 == s2:
+    # this is base case
+    # the first three would be based on the length of the two strings
+    ##################
+    if len(s1) == 0 and len(s2) == 0:
+        print(s1, s2, "equal")
         return 0
-#    elif s1 < s2:
+    elif len(s1) != 0 and len(s2) == 0:
+        print(s1, s2, "not equal")
+        return -1
+    elif len(s1) == 0 and len(s2) != 0:
+        print(s1, s2, "not equal")
+        return 1
+    elif s1[0] < s2[0]:
+        print(s1, s2, "not equal")
+        return -1
+    elif s1[0] > s2[0]:
+        print(s1, s2, "not equal")
+        return 1
+    ##################
+    # recursive case
+    else:
+        return compareTo(s1[1:], s2[1:])
 
-#    elif s1 > s2:
+
+def compareTo(s1, s2):
+    # this is base case
+    # ####################
+    if len(s1) == 0 and len(s2) == 0:
+        print(s1, s2, "equal")
+        return 0
+    elif len(s1) != 0 and len(s2) == 0:
+        print(s1, s2, "not equal")
+        return -1
+    elif len(s1) == 0 and len(s2) != 0:
+        print(s1, s2, "not equal")
+        return 1
+    elif s1[0] < s2[0]:
+        print(s1, s2, "not equal")
+        return -1
+    elif s1[0] > s2[0]:
+        print(s1, s2, "not equal")
+        return 1
+    ##################
+    # write recursive case here
+    else:
+        print(s1[0], s2[0], "both strings are equal")
+        return compareTo(s1[1:], s2[1:])
 
 
+# why 'len(s1) == 0 and len(s2) == 0' works
+# if you pass empty s1 and s2, it will directly return 0
+compareTo("", "")
+
+# if two strings are identical
+result = compareTo("apple", "apple")
+
+# here you need to check the result and print overall result, like
+if result == 0:
+    print("both strings are equal")
